840561485909e7b2fb8586d8e967aa79217bc199
daniel347x <daniel347x@gmail.com>
2013-05-06 00:53:11 -0400

    Fill in Singleton pattern for UIDocumentManager.

--- NewGeneUI/NewGene/Documents/uidocumentmanager.cpp
+++ NewGeneUI/NewGene/Documents/uidocumentmanager.cpp
@@ -1,6 +1,22 @@
 #include "uidocumentmanager.h"
 
+UIDocumentManager * UIDocumentManager::documentManager = NULL;
+
 UIDocumentManager::UIDocumentManager(QObject *parent) :
     QObject(parent)
 {
+}
+
+UIDocumentManager * UIDocumentManager::getDocumentManager()
+{
+    if (documentManager == NULL)
+    {
+        documentManager = new UIDocumentManager;
+    }
+    if (documentManager == NULL)
+    {
+        boost::format msg("Document manager not instantiated.");
+        throw NewGeneException() << newgene_error_description(msg.str());
+    }
+    return documentManager;
 }
--- NewGeneUI/NewGene/Documents/uidocumentmanager.h
+++ NewGeneUI/NewGene/Documents/uidocumentmanager.h
@@ -8,11 +8,14 @@ class UIDocumentManager : public QObject
     Q_OBJECT
 public:
     explicit UIDocumentManager(QObject *parent = 0);
-    
+
 signals:
-    
+
 public slots:
-    
+
+private:
+    static UIDocumentManager * documentManager;
+
 };
 
 #endif // UIDOCUMENTMANAGER_H
