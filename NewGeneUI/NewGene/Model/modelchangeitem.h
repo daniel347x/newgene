19bdcd5d59af95d686445a22034eb3073ae5c132
daniel347x <daniel347x@gmail.com>
2013-05-08 16:53:38 -0400

    Two additional files in the hierarchy committed a few minutes ago.

---
+++ NewGeneUI/NewGene/Model/modelchangeitem.h
@@ -0,0 +1,18 @@
+#ifndef MODELCHANGEITEM_H
+#define MODELCHANGEITEM_H
+
+#include <QObject>
+
+class ModelChangeItem : public QObject
+{
+		Q_OBJECT
+	public:
+		explicit ModelChangeItem(QObject *parent = 0);
+		
+	signals:
+		
+	public slots:
+		
+};
+
+#endif // MODELCHANGEITEM_H
