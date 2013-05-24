2b22e6aff035d007b1edbe8e4a0551b0afe55d6b
daniel347x <daniel347x@gmail.com>
2013-05-24 01:46:51 -0400

    Add stub backend InputModel and OutputModel classes, both derived from the currently-existing backend stub Model class.

---
+++ NewGeneBackEnd/Model/InputModel.h
@@ -0,0 +1,11 @@
+#ifndef INPUTMODEL_H
+#define INPUTMODEL_H
+
+#include "Model.h"
+
+class InputModel : public Model
+{
+
+};
+
+#endif
---
+++ NewGeneBackEnd/Model/OutputModel.h
@@ -0,0 +1,11 @@
+#ifndef OUTPUTMODEL_H
+#define OUTPUTMODEL_H
+
+#include "Model.h"
+
+class OutputModel : public Model
+{
+
+};
+
+#endif
