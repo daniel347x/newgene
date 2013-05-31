391f8a349746f94c3698da5c0104432906cf8769
daniel347x <daniel347x@gmail.com>
2013-05-31 09:52:36 -0400

    Make the path to Model class data const, because unlike settings, the Model location cannot easily change.

--- NewGeneBackEnd/Model/Model.h
+++ NewGeneBackEnd/Model/Model.h
@@ -13,25 +13,39 @@
 
 class Model
 {
-public:
-	Model(Messager & messager, boost::filesystem::path const path_to_model)
-	{
 
-	}
+	public:
+		
+		Model(Messager & messager, boost::filesystem::path const path_to_model)
+			: _path_to_model(path_to_model)
+		{
+
+		}
+
+		virtual void LoadModel(Messager & messager)
+		{
+
+		}
+
+	
+	protected:
+
+		boost::filesystem::path const _path_to_model;
+
 };
 
 template<typename MODEL_CLASS>
 class ModelFactory
 {
 
-public:
+	public:
 
-	MODEL_CLASS * operator()(Messager & messager, boost::filesystem::path const path_to_model)
-	{
-		MODEL_CLASS * new_model = new MODEL_CLASS(messager, path_to_model);
-		//new_model->LoadSettingsFromFile(messager);
-		return new_model;
-	}
+		MODEL_CLASS * operator()(Messager & messager, boost::filesystem::path const path_to_model)
+		{
+			MODEL_CLASS * new_model = new MODEL_CLASS(messager, path_to_model);
+			new_model->LoadModel(messager);
+			return new_model;
+		}
 
 };
 
