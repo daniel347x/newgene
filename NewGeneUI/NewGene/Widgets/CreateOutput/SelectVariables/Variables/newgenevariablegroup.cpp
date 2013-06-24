e088569bf03e1676b99c131cbb95cecc8f106a3c
daniel347x <daniel347x@gmail.com>
2013-06-24 03:50:06 -0400

    Checkboxes with variable names now displaying in Variable Group panes (dynamically).

--- NewGeneUI/NewGene/Widgets/CreateOutput/SelectVariables/Variables/newgenevariablegroup.cpp
+++ NewGeneUI/NewGene/Widgets/CreateOutput/SelectVariables/Variables/newgenevariablegroup.cpp
@@ -66,24 +66,39 @@ void NewGeneVariableGroup::RefreshAllWidgets()
 void NewGeneVariableGroup::WidgetDataRefreshReceive(WidgetDataItem_VARIABLE_GROUP_VARIABLE_GROUP_INSTANCE widget_data)
 {
 
-	std::string variables("Variables: ");
-
-	bool first = true;
-	std::for_each(widget_data.identifiers.cbegin(), widget_data.identifiers.cend(), [&variables, &first](DataInstanceIdentifier const & identifier)
+	if (!data_instance.uuid || !widget_data.identifier || !widget_data.identifier->uuid || (*data_instance.uuid) != (*widget_data.identifier->uuid) )
 	{
-		if (identifier.longhand)
+		boost::format msg("Invalid widget refresh in NewGeneVariableGroup widget.");
+		QMessageBox msgBox;
+		msgBox.setText( msg.str().c_str() );
+		msgBox.exec();
+		return;
+	}
+
+	//std::string variables("Variables: ");
+
+	//bool first = true;
+	std::for_each(widget_data.identifiers.cbegin(), widget_data.identifiers.cend(), [this /* , &variables, &first */ ](DataInstanceIdentifier const & identifier)
+	{
+		if (identifier.longhand && !identifier.longhand->empty() && ui->listWidget)
 		{
-			if (!first)
-			{
-				variables += ", ";
-			}
-			variables += *identifier.longhand;
-			first = false;
+
+			QListWidgetItem * variable_name_item = new QListWidgetItem(QString(identifier.longhand->c_str()), ui->listWidget);
+			variable_name_item->setCheckState(Qt::Unchecked);
+			ui->listWidget->addItem(variable_name_item);
+
+			//if (!first)
+			//{
+			//	variables += ", ";
+			//}
+			//variables += *identifier.longhand;
+			//first = false;
+
 		}
 	});
 
-	QMessageBox msgBox;
-	msgBox.setText( variables.c_str() );
-	msgBox.exec();
+	//QMessageBox msgBox;
+	//msgBox.setText( variables.c_str() );
+	//msgBox.exec();
 
 }
