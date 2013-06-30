853ef13f5a8af356ed6ee843b058232b5ec95cee
daniel347x <daniel347x@gmail.com>
2013-06-30 14:03:20 -0400

    Beginning of processing of response to user select variable action.  Test that data is passing through properly.

--- NewGeneBackEnd/UIAction/UIActionManager.cpp
+++ NewGeneBackEnd/UIAction/UIActionManager.cpp
@@ -17,13 +17,49 @@ void UIActionManager::DoVariableGroupSetMemberSelectionChange(Messager & message
 	OutputModel & output_model = project.model();
 	InputModel & input_model = project.model().getInputModel();
 
-	//switch (action_request.)
+	switch (action_request.reason)
+	{
+	case WIDGET_ACTION_ITEM_REQUEST_REASON__ADD_ITEMS:
+		{
 
-	//WidgetDataItem_VARIABLE_GROUPS_TOOLBOX variable_groups(widget_request);
-	//variable_groups.identifiers = input_model.t_vgp_identifiers.getIdentifiers();
+		}
+		break;
+	case WIDGET_ACTION_ITEM_REQUEST_REASON__REMOVE_ITEMS:
+		{
+
+		}
+		break;
+	case WIDGET_ACTION_ITEM_REQUEST_REASON__UPDATE_ITEMS:
+		{
+			for_each(action_request.items->cbegin(), action_request.items->cend(), [&input_model, &output_model, &messager](InstanceActionItem const & instanceActionItem)
+			{
+				if (!instanceActionItem.second)
+				{
+					return;
+				}
+				WidgetInstanceIdentifier const & identifier = instanceActionItem.first;
+				if (!identifier.uuid)
+				{
+					return;
+				}
+				WidgetActionItem const & actionItem = *instanceActionItem.second;
+				WidgetActionItem__Checkbox const & actionItemCheckbox = static_cast<WidgetActionItem__Checkbox const &>(actionItem);
+				UUID itemUUID = *identifier.uuid;
+				std::string itemCode;
+				if (identifier.code)
+				{
+					itemCode = *identifier.code;
+				}
+				boost::format msg("UUID = %1%, code = %2%");
+				msg % itemUUID % itemCode;
+				messager.ShowMessageBox(msg.str());
+			});
+		}
+		break;
+	}
 
 	//messager.EmitOutputWidgetDataRefresh(variable_groups);
 
-	messager.ShowMessageBox("Crazy crap");
+	//messager.ShowMessageBox("Crazy crap");
 
 }
