0bb24417b8f0ccb4708ea48c8d0517c449b556b9
daniel347x <daniel347x@gmail.com>
2013-07-25 15:40:31 -0400

    Integrating import of raw data into ModelManager.

--- NewGeneBackEnd/Model/ModelManager.cpp
+++ NewGeneBackEnd/Model/ModelManager.cpp
@@ -1,1 +1,304 @@
 #include "ModelManager.h"
+#include "InputModel.h"
+#include "OutputModel.h"
+
+bool ModelManager::ImportRawData(Messager & messager, Model_basemost * model_, TABLE_TYPES const table_type, boost::filesystem::path const & path_to_raw_data)
+{
+	try
+	{
+		InputModel * input_model = nullptr;
+		OutputModel * output_model = nullptr;
+		switch (table_type)
+		{
+			case TABLE__DMU_IDENTIFIER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__DMU_INSTANCE:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__CMU_IDENTIFIER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__CMU_INSTANCE:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__UOA_IDENTIFIER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__UOA_MEMBER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__VG_CATEGORY:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__VG_SET_MEMBER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__VG_SET_MEMBER_SELECTED:
+				{
+					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
+					if (output_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__KAD_COUNT:
+				{
+					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
+					if (output_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+		}
+		switch (table_type)
+		{
+			case TABLE__DMU_IDENTIFIER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__DMU_INSTANCE:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__CMU_IDENTIFIER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__CMU_INSTANCE:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__UOA_IDENTIFIER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__UOA_MEMBER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__VG_CATEGORY:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__VG_SET_MEMBER:
+				{
+					InputModel * input_model = dynamic_cast<InputModel*>(model_);
+					if (input_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__VG_SET_MEMBER_SELECTED:
+				{
+					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
+					if (output_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+			case TABLE__KAD_COUNT:
+				{
+					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
+					if (output_model)
+					{
+
+					}
+					else
+					{
+						// Todo: log warning
+						return false;
+					}
+				}
+				break;
+		}
+	}
+	catch (std::bad_cast &)
+	{
+		// Todo: log warning
+		return false;
+	}
+	return true;
+}
--- NewGeneBackEnd/Model/ModelManager.h
+++ NewGeneBackEnd/Model/ModelManager.h
@@ -2,9 +2,14 @@
 #define MODELMANAGER_H
 
 #include "..\Manager.h"
+#include "Tables\TableManager.h"
 
 class ModelManager : public Manager<ModelManager, MANAGER_DESCRIPTION_NAMESPACE::MANAGER_MODEL>
 {
+
+	public:
+
+		bool ImportRawData(Messager & messager, Model_basemost * model_, TABLE_TYPES const table_type, boost::filesystem::path const & path_to_raw_data);
 
 };
 
