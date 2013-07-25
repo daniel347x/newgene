bd55a263e6d577ca6cedf44d99696e3971687531
daniel347x <daniel347x@gmail.com>
2013-07-25 15:46:12 -0400

    Further en-route to integrating import with ModelManager.

--- NewGeneBackEnd/Model/ModelManager.cpp
+++ NewGeneBackEnd/Model/ModelManager.cpp
@@ -4,118 +4,25 @@
 
 bool ModelManager::ImportRawData(Messager & messager, Model_basemost * model_, TABLE_TYPES const table_type, boost::filesystem::path const & path_to_raw_data)
 {
+
+	InputModel * input_model = nullptr;
+	OutputModel * output_model = nullptr;
+
 	try
 	{
-		InputModel * input_model = nullptr;
-		OutputModel * output_model = nullptr;
 		switch (table_type)
 		{
 			case TABLE__DMU_IDENTIFIER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__DMU_INSTANCE:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__CMU_IDENTIFIER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__CMU_INSTANCE:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__UOA_IDENTIFIER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__UOA_MEMBER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__VG_CATEGORY:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__VG_SET_MEMBER:
 				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
+					input_model = dynamic_cast<InputModel*>(model_);
+					if (!input_model)
 					{
 						// Todo: log warning
 						return false;
@@ -123,27 +30,10 @@ bool ModelManager::ImportRawData(Messager & messager, Model_basemost * model_, T
 				}
 				break;
 			case TABLE__VG_SET_MEMBER_SELECTED:
-				{
-					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
-					if (output_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
 			case TABLE__KAD_COUNT:
 				{
-					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
-					if (output_model)
-					{
-
-					}
-					else
+					output_model = dynamic_cast<OutputModel*>(model_);
+					if (!output_model)
 					{
 						// Todo: log warning
 						return false;
@@ -151,154 +41,57 @@ bool ModelManager::ImportRawData(Messager & messager, Model_basemost * model_, T
 				}
 				break;
 		}
-		switch (table_type)
-		{
-			case TABLE__DMU_IDENTIFIER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
 
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__DMU_INSTANCE:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__CMU_IDENTIFIER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__CMU_INSTANCE:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__UOA_IDENTIFIER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__UOA_MEMBER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__VG_CATEGORY:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__VG_SET_MEMBER:
-				{
-					InputModel * input_model = dynamic_cast<InputModel*>(model_);
-					if (input_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__VG_SET_MEMBER_SELECTED:
-				{
-					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
-					if (output_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-			case TABLE__KAD_COUNT:
-				{
-					OutputModel * output_model = dynamic_cast<OutputModel*>(model_);
-					if (output_model)
-					{
-
-					}
-					else
-					{
-						// Todo: log warning
-						return false;
-					}
-				}
-				break;
-		}
 	}
 	catch (std::bad_cast &)
 	{
 		// Todo: log warning
 		return false;
 	}
+
+	switch (table_type)
+	{
+		case TABLE__DMU_IDENTIFIER:
+			{
+			}
+			break;
+		case TABLE__DMU_INSTANCE:
+			{
+			}
+			break;
+		case TABLE__CMU_IDENTIFIER:
+			{
+			}
+			break;
+		case TABLE__CMU_INSTANCE:
+			{
+			}
+			break;
+		case TABLE__UOA_IDENTIFIER:
+			{
+			}
+			break;
+		case TABLE__UOA_MEMBER:
+			{
+			}
+			break;
+		case TABLE__VG_CATEGORY:
+			{
+			}
+			break;
+		case TABLE__VG_SET_MEMBER:
+			{
+			}
+			break;
+		case TABLE__VG_SET_MEMBER_SELECTED:
+			{
+			}
+			break;
+		case TABLE__KAD_COUNT:
+			{
+			}
+			break;
+	}
+
 	return true;
 }
