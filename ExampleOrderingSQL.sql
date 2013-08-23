9c8d39909b9172e34a0832c1ac0c724ac8679a22
daniel347x <daniel347x@gmail.com>
2013-08-22 21:08:24 -0400

    Add comments to the example SQL that will be used as a template for ordering/sorting to eliminate duplicates.

--- ExampleOrderingSQL.sql
+++ ExampleOrderingSQL.sql
@@ -2,34 +2,46 @@ SELECT
   * 
 FROM
   VG_INSTANCE_DATA_VG_CTY_MID_DUMMY 
-WHERE (
+WHERE
+/* The following WHERE clause is semantically equivalent to the simpler clause "WHERE a <= b AND b <= c ...", with a, b, c, ...
+each corresponding to the set of primary key fields of the unit of analysis for that DMU category with multiplicity greater than 1
+(i.e. for the DMU category which the user has cranked up beyond the minimum via the spin control in the user interface),
+but it also handles the possibility of NULL for any of these primary key fields.
+This WHERE clause has the effect of including only rows where all NULL columns in primary keys fields appear to the right,
+and where the non-NULL primary keys increase from left to right.
+When there is more than one field in the unit of analysis corresponding to the DMU category with multiplicity greater than one, 
+the "<=" comparison is expanded to be based on the first field which is not equal. */ 
+(
     CASE
-      WHEN Dummy IS NULL 
-      AND DATETIME_ROW_START IS NULL 
+      WHEN Dummy IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
+      AND DATETIME_ROW_START IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
       THEN 1 
-      WHEN Dummy IS NULL 
-      AND DATETIME_ROW_START IS NOT NULL 
+      WHEN Dummy IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
+      AND DATETIME_ROW_START IS NOT NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
       THEN 0 
-      WHEN Dummy IS NOT NULL 
-      AND DATETIME_ROW_START IS NULL 
+      WHEN Dummy IS NOT NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
+      AND DATETIME_ROW_START IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
       THEN 1 
       ELSE Dummy <= DATETIME_ROW_START 
     END
   ) 
   AND (
     CASE
-      WHEN DATETIME_ROW_START IS NULL 
-      AND DATETIME_ROW_END IS NULL 
+      WHEN DATETIME_ROW_START IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
+      AND DATETIME_ROW_END IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
       THEN 1 
-      WHEN DATETIME_ROW_START IS NULL 
-      AND DATETIME_ROW_END IS NOT NULL 
+      WHEN DATETIME_ROW_START IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
+      AND DATETIME_ROW_END IS NOT NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
       THEN 0 
-      WHEN DATETIME_ROW_START IS NOT NULL 
-      AND DATETIME_ROW_END IS NULL 
+      WHEN DATETIME_ROW_START IS NOT NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
+      AND DATETIME_ROW_END IS NULL /* For units of analysis with more than one field in this DMU category for the primary key, checking the first field suffices as a NULL check */
       THEN 1 
       ELSE DATETIME_ROW_START <= DATETIME_ROW_END 
     END
-  ) 
+  )
+  /* ORDER BY the multiplicities of the DMU category with multiplicity greater than 1, from lowest to highest multiplicity.
+  If there is more than one primary key column per multiplicity, then order by the first field across multiplicities, 
+  then the second field across multiplicities, and so on. */
 ORDER BY Dummy,
   DATETIME_ROW_START,
   DATETIME_ROW_END 
