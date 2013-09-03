a4d56a875289cc6a7fa32d2b8e3789c5ed192618
daniel347x <daniel347x@gmail.com>
2013-09-03 02:19:27 -0400

    Implementing "Generate Output" pushbutton...

--- NewGeneUI/NewGene/Widgets/CreateOutput/GenerateOutput/newgenegenerateoutput.cpp
+++ NewGeneUI/NewGene/Widgets/CreateOutput/GenerateOutput/newgenegenerateoutput.cpp
@@ -3,12 +3,33 @@
 
 NewGeneGenerateOutput::NewGeneGenerateOutput(QWidget *parent) :
 	QWidget(parent),
+	NewGeneWidget( WidgetCreationInfo(this, parent, WIDGET_NATURE_OUTPUT_WIDGET, GENERATE_OUTPUT_TAB, true) ), // 'this' pointer is cast by compiler to proper Widget instance, which is already created due to order in which base classes appear in class definition
 	ui(new Ui::NewGeneGenerateOutput)
 {
+
 	ui->setupUi(this);
+
+	PrepareOutputWidget();
+
 }
 
 NewGeneGenerateOutput::~NewGeneGenerateOutput()
 {
 	delete ui;
+}
+
+void NewGeneGenerateOutput::UpdateOutputConnections(UIProjectManager::UPDATE_CONNECTIONS_TYPE connection_type, UIOutputProject * project)
+{
+	NewGeneWidget::UpdateOutputConnections(connection_type, project);
+	connect(this, SIGNAL(GenerateOutputSignal(WidgetActionItemRequest_ACTION_GENERATE_OUTPUT)), outp->getConnector(), SLOT(ReceiveVariableItemChanged(WidgetActionItemRequest_ACTION_KAD_COUNT_CHANGE)));
+}
+
+void NewGeneGenerateOutput::on_pushButtonGenerateOutput_clicked()
+{
+
+	InstanceActionItems actionItems;
+	actionItems.push_back(std::make_pair(WidgetInstanceIdentifier(), std::shared_ptr<WidgetActionItem>(static_cast<WidgetActionItem*>(new WidgetActionItem__GenerateOutput()))));
+	WidgetActionItemRequest_ACTION_GENERATE_OUTPUT action_request(WIDGET_ACTION_ITEM_REQUEST_REASON__DO_ACTION, actionItems);
+	emit GenerateOutputSignal(action_request);
+
 }
--- NewGeneUI/NewGene/Widgets/CreateOutput/GenerateOutput/newgenegenerateoutput.h
+++ NewGeneUI/NewGene/Widgets/CreateOutput/GenerateOutput/newgenegenerateoutput.h
@@ -1,22 +1,39 @@
 #ifndef NEWGENEGENERATEOUTPUT_H
 #define NEWGENEGENERATEOUTPUT_H
 
+#include "..\..\..\newgenewidget.h"
 #include <QWidget>
 
 namespace Ui {
 	class NewGeneGenerateOutput;
 }
 
-class NewGeneGenerateOutput : public QWidget
+class NewGeneGenerateOutput : public QWidget, public NewGeneWidget
 {
+
 		Q_OBJECT
-		
+
 	public:
+
 		explicit NewGeneGenerateOutput(QWidget *parent = 0);
 		~NewGeneGenerateOutput();
-		
+
 	private:
+
 		Ui::NewGeneGenerateOutput *ui;
+
+	public:
+
+	signals:
+
+		void GenerateOutputSignal(WidgetActionItemRequest_ACTION_GENERATE_OUTPUT);
+
+	public slots:
+
+		void UpdateOutputConnections(UIProjectManager::UPDATE_CONNECTIONS_TYPE connection_type, UIOutputProject * project);
+
+	private slots:
+		void on_pushButtonGenerateOutput_clicked();
 };
 
 #endif // NEWGENEGENERATEOUTPUT_H
