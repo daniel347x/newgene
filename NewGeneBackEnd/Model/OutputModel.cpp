1487aeb95c039d4ae4750e0efcb6a43d5f61c956
daniel347x <daniel347x@gmail.com>
2013-09-05 20:20:30 -0400

    Improve status text over stages

--- NewGeneBackEnd/Model/OutputModel.cpp
+++ NewGeneBackEnd/Model/OutputModel.cpp
@@ -8765,8 +8765,12 @@ void OutputModel::OutputGenerator::UpdateProgressBarToNextStage()
 	boost::format msg_1("Generating output to file %1%: Stage %2% of %3%");
 	msg_1 % boost::filesystem::path(setting_path_to_kad_output).filename() % ++current_progress_stage % total_progress_stages;
 	messager.UpdateStatusBarText(msg_1.str().c_str());
+
+	boost::format msg_1("Stage %1% of %2%");
+	msg_1 % ++current_progress_stage % total_progress_stages;
 	messager.AppendKadStatusText(msg_1.str().c_str());
-	messager.UpdateProgressBarValue(0);
+
+	messager.UpdateProgressBarValue(1000);
 }
 
 void OutputModel::OutputGenerator::CheckProgressUpdate(std::int64_t const current_rows_added_, std::int64_t const rows_estimate_, std::int64_t const starting_value_this_stage)
