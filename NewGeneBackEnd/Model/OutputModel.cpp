90022fde00584056bc7a1de884bebc514739dc77
daniel347x <daniel347x@gmail.com>
2013-09-09 19:38:13 -0400

    Also append current multiplicity to a different temporary table name.

--- NewGeneBackEnd/Model/OutputModel.cpp
+++ NewGeneBackEnd/Model/OutputModel.cpp
@@ -2580,6 +2580,11 @@ OutputModel::OutputGenerator::SqlAndColumnSet OutputModel::OutputGenerator::Remo
 	}
 	view_name += itoa(primary_group_number, c, 10);
 	result_columns.view_name_no_uuid = view_name;
+	if (current_multiplicity >= 0)
+	{
+		view_name += "_";
+		view_name += itoa(current_multiplicity, c, 10);
+	}
 	view_name += "_";
 	view_name += newUUID(true);
 	if (current_multiplicity >= 0)
