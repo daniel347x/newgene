d29934e4da46457d0044218ae5a701488bcff87e
daniel347x <daniel347x@gmail.com>
2013-09-08 20:01:56 -0400

    No timerange "WHERE" clause for merging of child variable groups because it leads to missing rows.

--- NewGeneBackEnd/Model/OutputModel.cpp
+++ NewGeneBackEnd/Model/OutputModel.cpp
@@ -7305,32 +7305,41 @@ OutputModel::OutputGenerator::SqlAndColumnSet OutputModel::OutputGenerator::Crea
 		});
 	});
 
-	if (!child_variable_group_raw_data_columns.has_no_datetime_columns_originally)
+	// No!  No WHERE clause for timerange when merging child groups!
+	// This has the effect of cause valid rows to not appear in the result set
+	// (i.e., the child data should just be NULL, rather than the entire row be missing).
+	// The later "XR" algorithm that loops through all rows evaluating the time range
+	// condition for both the previous, and the new, data, will take care of
+	// child data that does not match the user's selection of time range.
+	if (false)
 	{
-		sql_string += " WHERE CASE WHEN ";
-		sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 2].column_name_in_temporary_table;
-		sql_string += " = 0 AND ";
-		sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 1].column_name_in_temporary_table;
-		sql_string += " = 0 ";
-		sql_string += " THEN 1 ";
-		sql_string += " WHEN ";
-		sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 2].column_name_in_temporary_table;
-		sql_string += " < ";
-		sql_string += boost::lexical_cast<std::string>(timerange_end);
-		sql_string += " THEN ";
-		sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 1].column_name_in_temporary_table;
-		sql_string += " > ";
-		sql_string += boost::lexical_cast<std::string>(timerange_start);
-		sql_string += " WHEN ";
-		sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 1].column_name_in_temporary_table;
-		sql_string += " > ";
-		sql_string += boost::lexical_cast<std::string>(timerange_start);
-		sql_string += " THEN ";
-		sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 2].column_name_in_temporary_table;
-		sql_string += " < ";
-		sql_string += boost::lexical_cast<std::string>(timerange_end);
-		sql_string += " ELSE 0";
-		sql_string += " END";
+		if (!child_variable_group_raw_data_columns.has_no_datetime_columns_originally)
+		{
+			sql_string += " WHERE CASE WHEN ";
+			sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 2].column_name_in_temporary_table;
+			sql_string += " = 0 AND ";
+			sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 1].column_name_in_temporary_table;
+			sql_string += " = 0 ";
+			sql_string += " THEN 1 ";
+			sql_string += " WHEN ";
+			sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 2].column_name_in_temporary_table;
+			sql_string += " < ";
+			sql_string += boost::lexical_cast<std::string>(timerange_end);
+			sql_string += " THEN ";
+			sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 1].column_name_in_temporary_table;
+			sql_string += " > ";
+			sql_string += boost::lexical_cast<std::string>(timerange_start);
+			sql_string += " WHEN ";
+			sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 1].column_name_in_temporary_table;
+			sql_string += " > ";
+			sql_string += boost::lexical_cast<std::string>(timerange_start);
+			sql_string += " THEN ";
+			sql_string += result_columns.columns_in_view[result_columns.columns_in_view.size() - 2].column_name_in_temporary_table;
+			sql_string += " < ";
+			sql_string += boost::lexical_cast<std::string>(timerange_end);
+			sql_string += " ELSE 0";
+			sql_string += " END";
+		}
 	}
 
 	// For use in ORDER BY clause
