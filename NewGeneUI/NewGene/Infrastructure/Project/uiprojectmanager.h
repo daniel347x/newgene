88ca312888a8d744476c9427af1275246fd89064
daniel347x <daniel347x@gmail.com>
2013-12-15 07:51:03 -0500

    Constructor of additional class used in Clang workaround must have move ctor - perhaps this will finally successfully work around the Clang bug?

--- NewGeneUI/NewGene/Infrastructure/Project/uiprojectmanager.h
+++ NewGeneUI/NewGene/Infrastructure/Project/uiprojectmanager.h
@@ -164,7 +164,7 @@ class UIProjectManager : public QObject,
 		// ************************************************************************************************************************************* //
 		struct ProjectPaths
 		{
-			ProjectPaths(boost::filesystem::path const & project_settings_path_, boost::filesystem::path const & model_settings_path_, boost::filesystem::path const& model_database_path_)
+			ProjectPaths(boost::filesystem::path && project_settings_path_, boost::filesystem::path && model_settings_path_, boost::filesystem::path && model_database_path_)
 				: project_settings_path(project_settings_path_)
 				, model_settings_path(model_settings_path_)
 				, model_database_path(model_database_path_)
@@ -184,7 +184,6 @@ class UIProjectManager : public QObject,
 			ProjectPaths paths;
 			std::unique_ptr<UIProject<BACKEND_PROJECT_CLASS, UI_PROJECT_SETTINGS_CLASS, UI_MODEL_SETTINGS_CLASS, UI_MODEL_CLASS, UI_THREAD_LOOP_CLASS_ENUM>> project;
 
-			// Note: The following ctor does not work in conjunction with this class and its use in the application to overcome the Clang bug noted above
 			ProjectTabContents(ProjectPaths && paths_, std::unique_ptr<UIProject<BACKEND_PROJECT_CLASS, UI_PROJECT_SETTINGS_CLASS, UI_MODEL_SETTINGS_CLASS, UI_MODEL_CLASS, UI_THREAD_LOOP_CLASS_ENUM>> && project_)
 				: paths(std::move(paths_))
 				, project(std::move(project_))
