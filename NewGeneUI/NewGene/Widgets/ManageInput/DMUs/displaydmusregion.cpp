7ea34c3fd21b0695169bb857ea557da643573dbe
daniel347x <daniel347x@gmail.com>
2014-02-10 06:05:54 -0500

    First pass at displaying DMU's in DMU Manager pane

--- NewGeneUI/NewGene/Widgets/ManageInput/DMUs/displaydmusregion.cpp
+++ NewGeneUI/NewGene/Widgets/ManageInput/DMUs/displaydmusregion.cpp
@@ -1,6 +1,8 @@
 #include "displaydmusregion.h"
 #include "ui_displaydmusregion.h"
 
+#include <QStandardItem>
+
 #include "../Project/uiprojectmanager.h"
 #include "../Project/uiinputproject.h"
 
@@ -74,6 +76,58 @@ void DisplayDMUsRegion::WidgetDataRefreshReceive(WidgetDataItem_MANAGE_DMUS_WIDG
 	}
 
 	UIMessager messager(project);
+
+	if (!ui->listView)
+	{
+		boost::format msg("Invalid list view in NewGeneVariableGroup widget.");
+		QMessageBox msgBox;
+		msgBox.setText( msg.str().c_str() );
+		msgBox.exec();
+		return;
+	}
+
+	QStandardItemModel * oldModel = static_cast<QStandardItemModel*>(ui->listView->model());
+	if (oldModel != nullptr)
+	{
+		delete oldModel;
+	}
+
+	QItemSelectionModel * oldSelectionModel = ui->listView->selectionModel();
+	QStandardItemModel * model = new QStandardItemModel(ui->listView);
+
+	int index = 0;
+	std::for_each(widget_data.dmus_and_members.cbegin(), widget_data.dmus_and_members.cend(), [this, &index, &model](std::pair<WidgetInstanceIdentifier, WidgetInstanceIdentifiers> const & dmu_and_member)
+	{
+		WidgetInstanceIdentifier const & dmu = dmu_and_member.first;
+		WidgetInstanceIdentifiers const & dmu_members = dmu_and_member.second;
+		if (dmu.code && !dmu.code->empty())
+		{
+
+			QStandardItem * item = new QStandardItem();
+			QString text(dmu.code->c_str());
+			if (dmu.longhand && !dmu.longhand->empty())
+			{
+				text += " (";
+				text += dmu.longhand->c_str();
+				text += ")";
+			}
+			item->setText(text);
+			item->setEditable(false);
+			item->setCheckable(false);
+			QVariant v;
+			v.setValue(dmu_and_member);
+			item->setData(v);
+			model->setItem( index, item );
+
+			++index;
+
+		}
+	});
+
+	//connect(model, SIGNAL(itemChanged(QStandardItem*)), this, SLOT(ReceiveVariableItemChanged(QStandardItem*)));
+
+	ui->listView->setModel(model);
+	if (oldSelectionModel) delete oldSelectionModel;
 
 }
 
