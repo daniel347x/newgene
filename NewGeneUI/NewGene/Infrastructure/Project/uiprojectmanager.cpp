6cf6370c590bf633c283acd96b659de5a28533be
daniel347x <daniel347x@gmail.com>
2014-02-18 09:09:28 -0500

    Pointer dereference mismatch

--- NewGeneUI/NewGene/Infrastructure/Project/uiprojectmanager.cpp
+++ NewGeneUI/NewGene/Infrastructure/Project/uiprojectmanager.cpp
@@ -572,7 +572,7 @@ void UIProjectManager::RawOpenInputProject(UIMessager & messager, boost::filesys
 	// ************************************************************************************************************************************* //
 	std::unique_ptr<UIMessagerInputProject> messager_ptr(new UIMessagerInputProject(nullptr));
 	std::unique_ptr<UIInputProject> project_ptr(new UIInputProject(project_settings, model_settings, project_model, mainWindowObject, nullptr, *messager_ptr));
-	messager_ptr->set(*project_ptr);
+	messager_ptr->set(project_ptr);
 	input_tabs[mainWindow].emplace_back(ProjectPaths(input_project_settings_path, path_to_model_settings, path_to_model_database),
 		project_ptr.release(),
 		messager_ptr.release());
@@ -697,7 +697,7 @@ void UIProjectManager::RawOpenOutputProject(UIMessager & messager, boost::filesy
 
 	std::unique_ptr<UIMessagerOutputProject> messager_ptr(new UIMessagerOutputProject(nullptr));
 	std::unique_ptr<UIOutputProject> project_ptr(new UIOutputProject(project_settings, model_settings, project_model, mainWindowObject, nullptr, *messager_ptr));
-	messager_ptr->set(*project_ptr);
+	messager_ptr->set(project_ptr);
 	output_tabs[mainWindow].emplace_back(ProjectPaths(output_project_settings_path, path_to_model_settings, path_to_model_database),
 		project_ptr.release(),
 		messager_ptr.release());
