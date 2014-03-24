829e67cc9a0d070b0c055d983551611856e975a9
daniel347x <daniel347x@gmail.com>
2014-03-24 12:46:29 -0400

    Stub if/else conditions in "common case" time slice routine in place, with comments.

--- NewGeneBackEnd/Model/RandomSampling/RandomSampling.cpp
+++ NewGeneBackEnd/Model/RandomSampling/RandomSampling.cpp
@@ -88,6 +88,7 @@ void AllWeightings::AddLeafToTimeSlices(TimeSliceLeaf const & newTimeSliceLeaf)
 				{
 
 					// The new time slice starts at the left edge, or to the right of the left edge,
+					// of the first element in the map, but is to the left of the right edge
 					// of the first element in the map.
 
 					// This is actually the normal case - it turns out that it doesn't matter
@@ -110,14 +111,15 @@ void AllWeightings::AddLeafToTimeSlices(TimeSliceLeaf const & newTimeSliceLeaf)
 
 }
 
-bool AllWeightings::HandleTimeSliceNormalCase(TimeSliceLeaf & timeSliceLeaf, TimeSlices::iterator & mapElementPtr)
+bool AllWeightings::HandleTimeSliceNormalCase(TimeSliceLeaf & newTimeSliceLeaf, TimeSlices::iterator & mapElementPtr)
 {
 
 	// This function that takes the following arguments:
 	// - A time slice
 	// - An entry in the map
 	// The time slice's left edge is guaranteed to be inside or at the left edge of
-	// (not at the right edge of) an existing (non-"end()") entry of the map.
+	// (not at the right edge of) an existing (non-"end()") entry of the map -
+	// it is guaranteed not to be to the left of the left edge of the map entry.
 	// This function does the following:
 	// Processes the given entry in the map in relation to the time slice.
 	//     This involves either merging the entire slice into some or all
@@ -132,13 +134,118 @@ bool AllWeightings::HandleTimeSliceNormalCase(TimeSliceLeaf & timeSliceLeaf, Tim
 	//   that extends beyond the right edge of the map entry?
 	// The function returns the following data:
 	// If the return value is TRUE:
-	// - an empty TimeSliceLeaf
-	// - an iterator equal to "end()"
+	// - undefined values
 	// If the return value is FALSE:
 	// - The portion of the incoming slice that extends past the right edge
 	//   of the map entry
 	// - An iterator to the map entry to the right of the slice, if there is one,
 	//   or an iterator to one past the end of the map if there isn't one ("end()").
+
+	TimeSlice const & newTimeSlice = newTimeSliceLeaf.first;
+	TimeSlice const & mapElement   = mapElementPtr->first;
+
+	bool newTimeSliceEatenCompletelyUp = false;
+
+	if (newTimeSlice.time_start == mapElement.time_start)
+	{
+
+		// The new time slice starts at the left edge of the map element.
+
+		if (newTimeSlice.time_end < mapElement.time_end)
+		{
+
+			// The new time slice starts at the left edge of the map element,
+			// and fits inside the map element,
+			// with space left over in the map element.
+
+			// Slice the map element into two pieces.
+			// Merge the first piece with the new time slice.
+			// Leave the second piece unchanged.
+
+			newTimeSliceEatenCompletelyUp = true;
+
+		}
+		else if (newTimeSlice.time_end == mapElement.time_end)
+		{
+
+			// The new time slice exactly matches the first map element.
+
+			// Merge the new time slice with the map element.
+
+			newTimeSliceEatenCompletelyUp = true;
+
+		}
+		else
+		{
+
+			// The new time slice starts at the left edge of the map element,
+			// and extends past the right edge.
+
+			// Slice the first part of the time slice off so that it
+			// perfectly overlaps the map element.
+			// Merge it with the map element.
+
+		}
+
+	}
+	else
+	{
+
+		// The new time slice is inside the map element,
+		// but starts past its left edge.
+
+		if (newTimeSlice.time_end < mapElement.time_end)
+		{
+
+			// The new time slice starts in the map element,
+			// but past its left edge.
+			// The new time slice ends before the right edge of the map element.
+
+			// Slice the map element into three pieces.
+			// The first is unchanged.
+			// The second merges with the new time slice.
+			// The third is unchanged.
+
+			newTimeSliceEatenCompletelyUp = true;
+
+		}
+		else if (newTimeSlice.time_end == mapElement.time_end)
+		{
+
+			// The new time slice starts in the map element,
+			// but past its left edge.
+			// The new time slice ends at exactly the right edge of the map element.
+
+			// Slice the map element into two pieces.
+			// The first is unchanged.
+			// The second merges with the new time slice
+			// (with the right edge equal to the right edge of the map element).
+
+			newTimeSliceEatenCompletelyUp = true;
+
+		}
+		else
+		{
+
+			// The new time slice starts in the map element,
+			// but past its left edge.
+			// The new time slice ends past the right edge of the map element.
+
+			// Slice the left part of the time slice
+			// and the right part of the map element
+			// so that they are equal, and merge,
+
+		}
+
+	}
+
+	if (newTimeSliceEatenCompletelyUp)
+	{
+		return true;
+	}
+
+
+	return false;
 
 }
 
