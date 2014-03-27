f87764290b2d090390fb337dab5d3f3bea1eaa57
daniel347x <daniel347x@gmail.com>
2014-03-27 18:47:04 -0400

    Reversed ASSERT logic

--- NewGeneBackEnd/Model/RandomSampling/RandomSampling.cpp
+++ NewGeneBackEnd/Model/RandomSampling/RandomSampling.cpp
@@ -597,7 +597,7 @@ bool AllWeightings::RetrieveNextBranchAndLeaves(int const K, Branch & branch, Le
 
 	// random_number should be between 0 and the binomial coefficient representing the number of combinations of K leaves out of the total number of leaves
 	//BOOST_ASSERT_MSG((random_number - branch.weighting.weighting_range_start) < 0 || (random_number - branch.weighting.weighting_range_start) / boost::multiprecision::cpp_int(timeSlice.Width()) >= boost::math::binomial_coefficient<boost::multiprecision::cpp_int>(tmp_leaves.size(), K), "Random index is outside [0. binomial coefficient)");
-	BOOST_ASSERT_MSG((random_number - branch.weighting.getWeightingRangeStart()) < 0 || boost::multiprecision::cpp_int((random_number - branch.weighting.getWeightingRangeStart()) / boost::multiprecision::cpp_int(timeSlice.Width())) >= BinomialCoefficient(tmp_leaves.size(), K), "Random index is outside [0. binomial coefficient)");
+	BOOST_ASSERT_MSG((random_number - branch.weighting.getWeightingRangeStart()) >= 0 && boost::multiprecision::cpp_int((random_number - branch.weighting.getWeightingRangeStart()) / boost::multiprecision::cpp_int(timeSlice.Width())) < BinomialCoefficient(tmp_leaves.size(), K), "Random index is outside [0. binomial coefficient)");
 
 	// random_number is now an actual *index* to which combination of leaves in this VariableGroupTimeSliceData;
 	leaves = GetLeafCombination(K, branch, tmp_leaves);
