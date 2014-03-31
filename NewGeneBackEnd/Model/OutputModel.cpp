b2cc22d10cb2e3d722479e6d888ba17e8341edc3
daniel347x <daniel347x@gmail.com>
2014-03-30 23:15:52 -0400

    Carefully tweak the calling function to clarify which child leaf is being addressed in the called function of time slice merging of child variable groups

--- NewGeneBackEnd/Model/OutputModel.cpp
+++ NewGeneBackEnd/Model/OutputModel.cpp
@@ -21036,83 +21036,95 @@ void OutputModel::OutputGenerator::RandomSamplerFillDataForChildGroups(AllWeight
 
 		int const the_child_multiplicity = child_uoas__which_multiplicity_is_greater_than_1[*(child_variable_group_raw_data_columns.variable_groups[0].identifier_parent)].second;
 
+
+		// **************************************************************************************** //
+		// Populate the index mappings that map each child group's branch DMU data,
+		// and leaf DMU data, to a corresponding specific index in the
+		// top-level variable group's branch or a specific index in one of its specific leaves.
+		// **************************************************************************************** //
+
 		std::vector<ChildToPrimaryMapping> mappings_from_child_branch_to_primary;
 		std::vector<ChildToPrimaryMapping> mappings_from_child_leaf_to_primary;
 
-		int current_primary_branch_index = 0;
-		int current_primary_internal_leaf_index = 0;
-		int current_primary_leaf_number = 0;
-		std::for_each(sequence.primary_key_sequence_info.cbegin(), sequence.primary_key_sequence_info.cend(), [&](PrimaryKeySequence::PrimaryKeySequenceEntry const & full_kad_key_info)
 		{
 
-			bool is_primary_group_branch = false;
-			if (full_kad_key_info.total_outer_multiplicity__for_the_current_dmu_category__corresponding_to_the_uoa_corresponding_to_top_level_variable_group == 1)
-			{
-				is_primary_group_branch = true;
-			}
-
-			std::for_each(full_kad_key_info.variable_group_info_for_primary_keys.cbegin(),
-				full_kad_key_info.variable_group_info_for_primary_keys.cend(), [&](PrimaryKeySequence::VariableGroup_PrimaryKey_Info const & full_kad_key_info_this_variable_group)
+			int current_primary_branch_index = 0;
+			int current_primary_internal_leaf_index = 0;
+			int current_primary_leaf_number = 0;
+			std::for_each(sequence.primary_key_sequence_info.cbegin(), sequence.primary_key_sequence_info.cend(), [&](PrimaryKeySequence::PrimaryKeySequenceEntry const & full_kad_key_info)
 			{
 
-				if (full_kad_key_info_this_variable_group.vg_identifier.IsEqual(WidgetInstanceIdentifier::EQUALITY_CHECK_TYPE__STRING_CODE, child_variable_group_raw_data_columns.variable_groups[0]))
+				bool is_current_index_a_top_level_primary_group_branch = false;
+				if (full_kad_key_info.total_outer_multiplicity__for_the_current_dmu_category__corresponding_to_the_uoa_corresponding_to_top_level_variable_group == 1)
+				{
+					is_current_index_a_top_level_primary_group_branch = true;
+				}
+
+				std::for_each(full_kad_key_info.variable_group_info_for_primary_keys.cbegin(),
+					full_kad_key_info.variable_group_info_for_primary_keys.cend(), [&](PrimaryKeySequence::VariableGroup_PrimaryKey_Info const & full_kad_key_info_this_variable_group)
 				{
 
-					if (full_kad_key_info_this_variable_group.total_outer_multiplicity__in_total_kad__for_current_dmu_category__for_current_variable_group == 0)
+					if (full_kad_key_info_this_variable_group.vg_identifier.IsEqual(WidgetInstanceIdentifier::EQUALITY_CHECK_TYPE__STRING_CODE, child_variable_group_raw_data_columns.variable_groups[0]))
 					{
-						// There are no columns in this DMU for this variable group
-						return;
+
+						if (full_kad_key_info_this_variable_group.total_outer_multiplicity__in_total_kad__for_current_dmu_category__for_current_variable_group == 0)
+						{
+							// There are no columns in this DMU for this variable group
+							return;
+						}
+
+						bool is_child_group_branch = false;
+						if (full_kad_key_info_this_variable_group.total_outer_multiplicity__in_total_kad__for_current_dmu_category__for_current_variable_group == 1)
+						{
+							is_child_group_branch = true;
+						}
+
+						if (is_current_index_a_top_level_primary_group_branch && is_child_group_branch)
+						{
+							mappings_from_child_branch_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_BRANCH, current_primary_branch_index));
+						}
+						else
+						if (is_current_index_a_top_level_primary_group_branch && !is_child_group_branch)
+						{
+							mappings_from_child_leaf_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_BRANCH, current_primary_branch_index));
+						}
+						else
+						if (!is_current_index_a_top_level_primary_group_branch && is_child_group_branch)
+						{
+							mappings_from_child_branch_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_LEAF, current_primary_internal_leaf_index, current_primary_leaf_number));
+						}
+						else if (!is_current_index_a_top_level_primary_group_branch && !is_child_group_branch)
+						{
+							mappings_from_child_leaf_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_LEAF, current_primary_internal_leaf_index, current_primary_leaf_number));
+						}
+						else
+						{
+							boost::format msg("No mapping possible from child to primary DMU!");
+							throw NewGeneException() << newgene_error_description(msg.str());
+						}
+
 					}
 
-					bool is_child_group_branch = false;
-					if (full_kad_key_info_this_variable_group.total_outer_multiplicity__in_total_kad__for_current_dmu_category__for_current_variable_group == 1)
-					{
-						is_child_group_branch = true;
-					}
+				});
 
-					if (is_primary_group_branch && is_child_group_branch)
+				if (is_current_index_a_top_level_primary_group_branch)
+				{
+					++current_primary_branch_index;
+				}
+				else
+				{
+					++current_primary_internal_leaf_index;
+					if (full_kad_key_info.sequence_number_within_dmu_category_primary_uoa + 1 == full_kad_key_info.total_k_count_within_high_level_variable_group_uoa_for_this_dmu_category)
 					{
-						mappings_from_child_branch_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_BRANCH, current_primary_branch_index));
+						++current_primary_leaf_number;
+						current_primary_internal_leaf_index = 0;
 					}
-					else
-					if (is_primary_group_branch && !is_child_group_branch)
-					{
-						mappings_from_child_leaf_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_BRANCH, current_primary_branch_index));
-					}
-					else
-					if (!is_primary_group_branch && is_child_group_branch)
-					{
-						mappings_from_child_branch_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_LEAF, current_primary_internal_leaf_index, current_primary_leaf_number));
-					}
-					else if (!is_primary_group_branch && !is_child_group_branch)
-					{
-						mappings_from_child_leaf_to_primary.push_back(ChildToPrimaryMapping(CHILD_TO_PRIMARY_MAPPING__MAPS_TO_LEAF, current_primary_internal_leaf_index, current_primary_leaf_number));
-					}
-					else
-					{
-						boost::format msg("No mapping possible from child to primary DMU!");
-						throw NewGeneException() << newgene_error_description(msg.str());
-					}
-
 				}
 
 			});
 
-			if (is_primary_group_branch)
-			{
-				++current_primary_branch_index;
-			}
-			else
-			{
-				++current_primary_internal_leaf_index;
-				if (full_kad_key_info.sequence_number_within_dmu_category_primary_uoa + 1 == full_kad_key_info.total_k_count_within_high_level_variable_group_uoa_for_this_dmu_category)
-				{
-					++current_primary_leaf_number;
-					current_primary_internal_leaf_index = 0;
-				}
-			}
 
-		});
+		}
 
 		for (int current_leaf_index = 0; current_leaf_index < the_child_multiplicity; ++current_leaf_index)
 		{
