42078be1c925e862ad7efcdd32139944b13fd8ee
daniel347x <daniel347x@gmail.com>
2014-04-03 09:36:27 -0400

    Warning comment in incomplete function.

--- NewGeneBackEnd/Model/OutputModel.cpp
+++ NewGeneBackEnd/Model/OutputModel.cpp
@@ -515,6 +515,10 @@ void OutputModel::OutputGenerator::GenerateOutput(DataChangeMessage & change_res
 			RandomSamplingCreateOutputTable();
 			if (failed || CheckCancelled()) return;
 
+			// *********************************************************************** //
+			// The following function has incomplete logic, and should
+			// be merged with RandomSamplingWriteResultsToFileOrScreen().
+			// *********************************************************************** //
 			RandomSamplingWriteToOutputTable(allWeightings, errorMessages);
 			if (failed || CheckCancelled()) return;
 
