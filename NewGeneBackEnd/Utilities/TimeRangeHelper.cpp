20ddd745d9cd19f00cb5dfe99fb0674c98aba4e3
daniel347x <daniel347x@gmail.com>
2014-04-04 19:48:27 -0400

    First case of boost::posix_time logic in place for "nearest aligning time" function

--- NewGeneBackEnd/Utilities/TimeRangeHelper.cpp
+++ NewGeneBackEnd/Utilities/TimeRangeHelper.cpp
@@ -15,12 +15,40 @@ std::int64_t TimeRange::determineNextHighestAligningTimestamp(std::int64_t const
 		throw NewGeneException() << newgene_error_description(msg.str());
 	}
 
+	namespace bpt = boost::posix_time;
+
+	bpt::ptime incoming_time = bpt::from_time_t(test_timestamp / 1000);
+	bpt::ptime time_t_epoch__1970(boost::gregorian::date(1970, 1, 1));
+
 	switch (time_granularity)
 	{
 
 		case TIME_GRANULARITY__SECOND:
 			{
-				result = second;
+				// Get number of seconds into the day represented by incoming_time
+				bpt::time_duration seconds_into_day = bpt::seconds(incoming_time.time_of_day().total_seconds());
+				bpt::time_duration milliseconds_into_day = bpt::seconds(incoming_time.time_of_day().total_milliseconds());
+
+				// Use the difference between the time duration in seconds
+				// and the time duration in milliseconds
+				// to round to the nearest second
+				bpt::time_duration difference_seconds_milliseconds = milliseconds_into_day - seconds_into_day;
+				bool round_up = false;
+				if (difference_seconds_milliseconds.total_milliseconds() > 500)
+				{
+					round_up = true;
+				}
+
+				// Set the result, but rounded *down* to the nearest second
+				bpt::ptime result(incoming_time.date(), seconds_into_day);
+				if (round_up)
+				{
+					result += bpt::seconds(1);
+				}
+
+				boost::posix_time::time_duration ms_epoch = result - time_t_epoch__1970;
+
+				return ms_epoch.total_milliseconds();
 			}
 			break;
 
