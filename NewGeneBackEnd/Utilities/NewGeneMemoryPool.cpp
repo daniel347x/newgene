8fd79134277a0fd3421da638f836aa8fb54c2c6c
daniel347x <daniel347x@gmail.com>
2014-04-12 13:12:45 -0400

    Logic mistake in bitmask operation to determine free memory addresses in custom pool allocator I just whipped off.

--- NewGeneBackEnd/Utilities/NewGeneMemoryPool.cpp
+++ NewGeneBackEnd/Utilities/NewGeneMemoryPool.cpp
@@ -4,17 +4,21 @@ NewGenePool * NewGenePool::existingMaps[MAX_ITEM_SIZE_IN_BYTES/4];
 
 char * NewGenePool::CheckReturnFreeSlot()
 {
-	if ((blockbits[previous_block_holding_deleted_item_index + previous_index_to_deleted_item / 8] & (static_cast<char>((0x01 << (previous_index_to_deleted_item % 8))))) == 0)
+
+	if ((blockbits[previous_block_holding_deleted_item_index + previous_index_to_deleted_item / 8] & (static_cast<char>((0x01 << (previous_index_to_deleted_item % 8))))) != 0)
 	{
-		// it is a free slot.  Set slot to not free, and return pointer.
-		blockbits[previous_block_holding_deleted_item_index + previous_index_to_deleted_item / 8] |= static_cast<char>((0x01 << (previous_index_to_deleted_item % 8)));
-		char * ret = previous_block_holding_deleted_item + previous_index_to_deleted_item * mySize;
-		++previous_index_to_deleted_item;
-		if (previous_index_to_deleted_item == BLOCK_ITEM_COUNT)
-		{
-			previous_index_to_deleted_item = 0;
-		}
-		return ret;
+		// The bit is set: it is not a free slot
+		return nullptr;
 	}
-	return nullptr;
+
+	// The bit is unset: it is a free slot.  Set slot to not free, and return pointer.
+	blockbits[previous_block_holding_deleted_item_index + previous_index_to_deleted_item / 8] |= static_cast<char>((0x01 << (previous_index_to_deleted_item % 8)));
+	char * ret = previous_block_holding_deleted_item + previous_index_to_deleted_item * mySize;
+	++previous_index_to_deleted_item;
+	if (previous_index_to_deleted_item == BLOCK_ITEM_COUNT)
+	{
+		previous_index_to_deleted_item = 0;
+	}
+	return ret;
+
 }
