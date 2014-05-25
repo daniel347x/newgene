984e14bcfed60cba83e4e21ce638187321a6ae5c
Dan Nissenbaum <daniel347x@gmail.com>
2014-05-25 08:27:55 -0400

    En-route to tyin in action handling for Limit DMU functionality

--- NewGeneBackEnd/UIAction/Controllers/Controller_LimitDmus.cpp
+++ NewGeneBackEnd/UIAction/Controllers/Controller_LimitDmus.cpp
@@ -37,7 +37,54 @@ void UIActionManager::DoLimitDmusChange(Messager & messager, WidgetActionItemReq
 		case WIDGET_ACTION_ITEM_REQUEST_REASON__ADD_ITEMS:
 			{
 
-			}
+															 DataChangeMessage change_response(&project);
+
+															 for_each(action_request.items->cbegin(), action_request.items->cend(), [&input_model, &output_model, &messager, &change_response](InstanceActionItem const & instanceActionItem)
+															 {
+
+																 Executor executor(input_model.getDb());
+
+																 if (!instanceActionItem.second)
+																 {
+																	 return;
+																 }
+
+																 WidgetInstanceIdentifier const & identifier = instanceActionItem.first;
+
+																 if (!identifier.uuid)
+																 {
+																	 return;
+																 }
+
+																 // ************************************* //
+																 // Retrieve data sent by user interface
+																 // ************************************* //
+																 WidgetActionItem const & actionItem = *instanceActionItem.second;
+																 WidgetActionItem__Spinbox const & actionItemSpinbox = static_cast<WidgetActionItem__Spinbox const &>(actionItem);
+
+																 // ***************************************** //
+																 // Prepare data to send back to user interface
+																 // ***************************************** //
+																 DATA_CHANGE_TYPE type = DATA_CHANGE_TYPE__OUTPUT_MODEL__KAD_COUNT_CHANGE;
+																 DATA_CHANGE_INTENTION intention = DATA_CHANGE_INTENTION__UPDATE;
+																 WidgetInstanceIdentifiers child_identifiers;
+																 child_identifiers.push_back(identifier);
+																 DataChange change(type, intention, WidgetInstanceIdentifier(identifier), child_identifiers);
+																 change.SetPacket(std::make_shared<DataChangePacket_int>(actionItemSpinbox.getValue()));
+																 change_response.changes.push_back(change);
+
+																 // ***************************************** //
+																 // Update database and cache
+																 // ***************************************** //
+																 output_model.t_kad_count.Update(output_model.getDb(), output_model, input_model, change_response);
+
+																 executor.success();
+
+															 });
+
+															 messager.EmitChangeMessage(change_response);
+		
+		}
 			break;
 
 		case WIDGET_ACTION_ITEM_REQUEST_REASON__REMOVE_ITEMS:
@@ -49,52 +96,6 @@ void UIActionManager::DoLimitDmusChange(Messager & messager, WidgetActionItemReq
 		case WIDGET_ACTION_ITEM_REQUEST_REASON__UPDATE_ITEMS:
 			{
 
-				DataChangeMessage change_response(&project);
-
-				for_each(action_request.items->cbegin(), action_request.items->cend(), [&input_model, &output_model, &messager, &change_response](InstanceActionItem const & instanceActionItem)
-				{
-
-					Executor executor(input_model.getDb());
-
-					if (!instanceActionItem.second)
-					{
-						return;
-					}
-
-					WidgetInstanceIdentifier const & identifier = instanceActionItem.first;
-
-					if (!identifier.uuid)
-					{
-						return;
-					}
-
-					// ************************************* //
-					// Retrieve data sent by user interface
-					// ************************************* //
-					WidgetActionItem const & actionItem = *instanceActionItem.second;
-					WidgetActionItem__Spinbox const & actionItemSpinbox = static_cast<WidgetActionItem__Spinbox const &>(actionItem);
-
-					// ***************************************** //
-					// Prepare data to send back to user interface
-					// ***************************************** //
-					DATA_CHANGE_TYPE type = DATA_CHANGE_TYPE__OUTPUT_MODEL__KAD_COUNT_CHANGE;
-					DATA_CHANGE_INTENTION intention = DATA_CHANGE_INTENTION__UPDATE;
-					WidgetInstanceIdentifiers child_identifiers;
-					child_identifiers.push_back(identifier);
-					DataChange change(type, intention, WidgetInstanceIdentifier(identifier), child_identifiers);
-					change.SetPacket(std::make_shared<DataChangePacket_int>(actionItemSpinbox.getValue()));
-					change_response.changes.push_back(change);
-
-					// ***************************************** //
-					// Update database and cache
-					// ***************************************** //
-					output_model.t_kad_count.Update(output_model.getDb(), output_model, input_model, change_response);
-
-					executor.success();
-
-				});
-
-				messager.EmitChangeMessage(change_response);
 
 			}
 			break;
