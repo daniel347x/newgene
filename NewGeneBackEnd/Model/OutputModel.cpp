f366bfee0dd6679f6b347d80e745956413361373
daniel347x <daniel347x@gmail.com>
2014-06-07 22:34:44 -0400

    Be sure to clear branch/leaf flags when resetting data row while looping through raw data

--- NewGeneBackEnd/Model/OutputModel.cpp
+++ NewGeneBackEnd/Model/OutputModel.cpp
@@ -858,6 +858,9 @@ void OutputModel::OutputGenerator::SavedRowData::Clear()
 	is_index_a_secondary_key.clear();
 	indices_of_secondary_key_columns.clear();
 	indices_of_all_columns.clear();
+	rowid = n0;
+	branch_has_excluded_dmu = false;
+	leaf_has_excluded_dmu = false;
 }
 
 void OutputModel::OutputGenerator::DetermineInternalChildLeafCountMultiplicityGreaterThanOne(KadSampler & allWeightings, ColumnsInTempView const & column_schema,
